4110607a0c619ecc8e169c5a3b950f7ce0daf6ca
ashrafulhossain1 <ahmeadashraful@gmail.com>
2024-09-19 11:47:20 +0600

    add money btn and get input ammount and pin

--- js/home.js
+++ js/home.js
@@ -1,1 +1,12 @@
-console.log('home added')
+
+
+// add money
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        //1. prevent page reload 
+        event.preventDefault();
+        //2. get input amount and PIN
+        const addMoneyInput = document.getElementById('input-add-money').value;
+        const pinNumber = document.getElementById('pin-number').value;
+        console.log(addMoneyInput,pinNumber)
+    })
